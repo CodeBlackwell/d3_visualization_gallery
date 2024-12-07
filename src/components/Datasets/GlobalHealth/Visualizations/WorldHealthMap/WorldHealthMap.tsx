904325b7f0537362e919e4ea8477d43a1f3512e1
LeChristopher J Blackwell <Codeblackwell@gmail.com>
2024-12-07 02:06:27 -0700

    CSS centered map

--- src/components/Datasets/GlobalHealth/Visualizations/WorldHealthMap/WorldHealthMap.tsx
+++ src/components/Datasets/GlobalHealth/Visualizations/WorldHealthMap/WorldHealthMap.tsx
@@ -54,6 +54,8 @@ const WorldHealthMap: React.FC<WorldHealthMapProps> = ({ className, data = [], m
         .append('svg')
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
+        .attr('preserveAspectRatio', 'xMidYMid meet')
+        .attr('viewBox', `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
         .append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
